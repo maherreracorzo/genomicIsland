fbb7eab76e744d069cef839fdf5fad7ab0685c1d
Mariana Herrera Corzo <maherreracorzo@gmail.com>
2022-10-21 07:22:38 -0400

    filename and bash script

--- annotate_and_get_islanders_example.sh
+++ annotate_and_get_islanders_example.sh
@@ -27,26 +27,25 @@ echo "PROGRAM BEGINS" >> run_log.txt
 cd ..
 
 ##list1 is a list of the name of your genomes
-cat ./xp_example.txt | while read x
+cat ./All_Xp_Strains.txt | while read x
 do
 
 echo "ITERATION $x"
-echo "ITERATION $x" >> ./genomicIsland/run_log.txt
+echo "ITERATION $x" >> ./genomicIsland/run_log.txt;
 
-#for x in `$(cat ../xp_example.txt)`
-#do
 
 ##concatenate contigs
 #ls $x".fasta"
 #sed 's/^>.*$/NNN/g' $x".fasta" | awk 'BEGIN { ORS=""; print ">contigs\n" } { print }' > $x"cat_contigs.fasta"
 
 ##prokka
-
-#prokka --cpus 8 --locustag $x --outdir "prokka_"$x --prefix $x $x"cat_contigs.fasta"
+prokkafile=`echo "replacecat_contigs.fasta" | sed -r 's/replace/'${x}'/g'`
+echo "$prokkafile"
+prokka --cpus 8 --locustag $x --outdir "prokka_"$x --prefix ${x} $prokkafile
 
 ##alien_hunter
 
-#alien_hunter $x"cat_contigs.fasta" $x
+alien_hunter ${x}cat_contigs.fasta ${x}
 
 ##grep the genes overlappimg the islands
 
@@ -55,12 +54,9 @@ grep mis ${x}.sco | sed 's/\../\t/g' | sed 's/FT/contigs/g' |sed 's/\misc_featur
 
 cat /blue/goss/marianaherreraco/alien_hunter-1.7/Concat_annot_ah/examplejun23/prokka_${x}/${x}.gff | grep -v '#'|grep '^contigs' | cut -f 1,4,5,9 > /blue/goss/marianaherreraco/alien_hunter-1.7/Concat_annot_ah/examplejun23/prokka_${x}/${x}_1.gff
 
-#cat jk10-02island.bed |sed 's/ //g' > experimento.bed
 bedtools intersect -a prokka_${x}/${x}_1.gff -b ${x}island.bed > ${x}_islanders.gff
 
-#bedtools intersect -a prokka_${x}/${x}_1.gff -b experimento.bed > ${x}_islanders.gff
 
-#sort ${x}_islanders.gff | uniq  > ${x}_islandersuniq.gff
 cat ${x}_islanders.gff | cut -f 4 | cut -f 1 -d ";" | sed 's/ID=//g' > ${x}_islanders_ID.tab
 sort ${x}_islanders_ID.tab | uniq  > ${x}_islandersuniq.tab
 seqtk subseq prokka_${x}/${x}.faa  ${x}_islanders_ID.tab > ${x}_islanders_ID.faa
